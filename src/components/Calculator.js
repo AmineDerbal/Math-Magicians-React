83e0bf76f73333e69f17693c2c87e83818ca0280
Amine derbal <aminedoran31@gmail.com>
2023-03-20 13:40:25 +0100

    add calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './style/Calculator.css';
+
+const Calculator = () => (
+  <div id="calculator-body">
+    <div id="calculator-screen">
+      <p>0</p>
+    </div>
+    <div id="calculator-buttons">
+      <button type="button">AC</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button" className="orange-button">
+        ÷
+      </button>
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button" className="orange-button">
+        x
+      </button>
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button" className="orange-button">
+        -
+      </button>
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button" className="orange-button">
+        +
+      </button>
+      <button type="button" id="zero">
+        0
+      </button>
+      <button type="button">.</button>
+      <button type="button" className="orange-button">
+        =
+      </button>
+    </div>
+  </div>
+);
+
+export default Calculator;
