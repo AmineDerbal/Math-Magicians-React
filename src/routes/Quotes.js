5ae9cdc8b058ce16f02d779f91d33405e5a5eac0
Amine derbal <aminedoran31@gmail.com>
2023-03-27 15:23:27 +0100

    move to route folder

---
+++ src/routes/Quotes.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import apiKey from '../components/apiKey';
+import Loader from '../components/loader';
+import '../styles/Api.css';
+
+const Quotes = () => {
+  const [data, setData] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const dataJson = await axios('https://api.api-ninjas.com/v1/quotes?category=computers', {
+          headers: {
+            'x-api-key': apiKey,
+          },
+        });
+        const response = await dataJson.data;
+
+        setData(response[0]);
+      } catch (error) {
+        setHasError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+    return () => {
+      setIsLoading(false);
+    };
+  }, [setData, setIsLoading]);
+
+  if (hasError) return <p className="error">Something went wrong!</p>;
+  if (isLoading) return <Loader />;
+
+  return (
+    <div className="quotes-data">
+      <p>
+        <strong>{data.quote}</strong>
+        {' '}
+        <span>
+          ~
+          {data.author}
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Quotes;
