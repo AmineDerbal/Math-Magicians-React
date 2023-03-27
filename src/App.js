8d926acf1fc1c592fa739b155fa587c1faad08d4
Amine derbal <aminedoran31@gmail.com>
2023-03-27 15:24:48 +0100

    Add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import Calculator from './components/Calculator';
-import Quotes from './components/Quotes';
+import {
+  BrowserRouter as Router, Routes, Route, Link,
+} from 'react-router-dom';
+import Home from './routes/Home';
+import CalculatorRoute from './routes/CalculatorRoute';
+import Quotes from './routes/Quotes';
+import Error from './routes/Error';
 
 const App = () => (
-  <div>
-    <Calculator />
-    <Quotes />
-  </div>
+  <Router>
+    <div>
+      <header id="header">
+        <h1>Math Magicians</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/Calculator">Calculator</Link>
+          <Link to="/Quote">Quote</Link>
+        </nav>
+      </header>
+    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/Calculator" element={<CalculatorRoute />} />
+      <Route path="/Quote" element={<Quotes />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
