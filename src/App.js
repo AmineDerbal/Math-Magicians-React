6d7887ca34edd868ad88962fc602d44a8db17f1f
Amine derbal <aminedoran31@gmail.com>
2023-03-28 17:02:11 +0100

    update component rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import {
-  BrowserRouter as Router, Routes, Route, Link,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import CalculatorRoute from './routes/CalculatorRoute';
 import Quotes from './routes/Quotes';
 import Error from './routes/Error';
+import Header from './components/Header';
 
 const App = () => (
   <Router>
-    <div>
-      <header id="header">
-        <h1>Math Magicians</h1>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/Calculator">Calculator</Link>
-          <Link to="/Quote">Quote</Link>
-        </nav>
-      </header>
-    </div>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Calculator" element={<CalculatorRoute />} />
