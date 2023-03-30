8626cc835bdf3feb4802abd29dcc87cdc838ad6e
Amine derbal <aminedoran31@gmail.com>
2023-03-30 10:30:23 +0100

    update strict mode

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import { React, StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { StrictMode } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
